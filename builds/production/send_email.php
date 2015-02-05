ef491240d882562b0cd757009e9e8a5a5ccd45f7
Uncle Ko <ktee55@gmail.com>
2015-02-05 20:14:53 +0900

    Almost Done!!|

--- builds/production/send_email.php
+++ builds/production/send_email.php
@@ -1,30 +1,42 @@
-<html>
-<body>
-
 <?php
-	if (isset($_POST['email']))
-	//if "email" is filled out, send email
-	{
-		//send email
-		$email = $_POST['email'] ;
-		$name= $_POST['name'] ;
-		$comments = $_POST['comments'] ;
 
-		$message="You've got a inquiry from your prospective client!!\n\n";
+	if (isset($_POST['name'])) {
+	
+		$name = strip_tags($_POST['name']);
+		$email= strip_tags($_POST['email']);		
+		$subject = strip_tags($_POST['subject']);
+		$comments = strip_tags($_POST['comments']);
+		
+		$message="お問い合わせが届きました。\n\n";
 		$message.= "Name: ".$name."\n";
 		$message.="Email: ".$email."\n";
 		$message.="Comments: ".$comments;
+		
+		if (validate_email($email) && !empty($name) && !empty($subject) && !empty($comments) ) {
+		
+			if (mail('info@ktee8.com', $subject, $message, "From: $email")) {
+				echo "<h2>Your message has been sent. Thanks!!</h2>
+							<h3>メッセージが送信されました。ありがとうございました。</h3>
+							<h3>できるだけ早く返信致しますのでしばしお待ちください。</h3>";
+			} else {
+				echo "<h2>Problem occured while sending email</h2>
+							<h3>送信中に何らかのトラブルが発生しました。大変申し訳ございませんがもう一度お試しください。</h3>";
+			}
+		
+		} else {
+		
+			echo "<h2>Please fill out all fields and ensure that your email address is valid.</h2>
+						<h3>フォーム内の全ての項目を記入ください。また、メールアドレスが正しくない可能性もあります。</h3>";
 
-		mail("info@shrewd-design.com", "Inquiry",
-		$message, "From:" . $email);
-		echo "<h1>Thank you. We'll be<br> in touch with you <br>shortly!</h1>";
+		}
+	
+	} else {
+		echo "<h2>Please fill out all fields and ensure that your email address is valid.</h2>
+					<h3>フォーム内の全ての項目を記入ください。また、メールアドレスが正しくない可能性もあります。</h3>";
 	}
-	else
-	//if "email" is not filled out, display the form
-	{
-		echo "<h1>There was a problem. Please  try again.</h1>";
+	
+	function validate_email($email) {
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
 	}
+	
 ?>
-
-</body>
-</html>
